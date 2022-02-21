cb427ec2c0363268a911d0bcf99a56a8da44307d
payamd <payam.d7@gmail.com>
2022-02-20 21:39:38 -0900

    shop's item Services and model

---
+++ Models/ShopItem.cs
@@ -0,0 +1,27 @@
+namespace Merchant_API.models;
+
+public class ShopItem{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string ShortDescription { get; set; }
+    public string Description { get; set; }
+    public string Picture { get; set; }
+    public string Price { get; set; }
+    public string Option { get; set; }
+    public string Quantity { get; set; }
+
+    public ShopItem (string Id, string Name, string ShortDescription,
+    string Description, string Picture,string Price, string Option, string Quantity)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.ShortDescription = ShortDescription;
+        this.Description = Description;
+        this.Picture = Picture;
+        this.Price = Price;
+        this.Option = Option;
+        this.Quantity = Quantity;
+
+        
+    }
+}
---
+++ Services/ShopItemService.cs
@@ -0,0 +1,76 @@
+using Merchant_API.models;
+
+namespace Merchant_API.services;
+
+public class ShopItemService{
+
+    /// ctor
+    public ShopItemService()
+    {
+        
+    }
+
+
+///Default Values
+    private List<ShopItem> ShopItems= new List<ShopItem> () {
+        new ShopItem("1", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("2", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("3", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("4", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("5", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("6", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("7", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("8", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("9", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1"),
+        new ShopItem("10", "Item 1 name", "Short description","Long description","Picture","Price","Black", "1")
+
+    };
+
+/// Create Method
+public async Task CreateAsynce (ShopItem newShopItem){
+    ShopItems.Add(newShopItem);
+}
+
+/// Get all method
+public async Task<List<ShopItem>> GetAsync(){
+    return ShopItems;
+}
+
+/// get one method
+public async Task<ShopItem> GetAsync( string Id){
+
+    return ShopItems.Find(x => x.Id == Id);
+}
+/// Update method
+public async Task<bool> UpdateAsync (string Id, ShopItem UpdatedShopItem){
+    bool result = false;
+    int index = ShopItems.FindIndex(x=> x.Id == Id);
+    if (index != -1){
+        UpdatedShopItem.Id = Id;
+        ShopItems[index]= UpdatedShopItem;
+        result=true;
+    }
+
+    return result;
+
+}
+
+
+/// Detele method
+public async Task<bool> DeleteAsync(string Id){
+    bool result = false;
+    int index = ShopItems.FindIndex(x=> x.Id == Id);
+    if (index != -1){
+        ShopItems.RemoveAt(index);
+        result=true;
+    }
+
+    return result;
+
+}
+
+
+
+
+
+}
