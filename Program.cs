eeed8056a0c1a9082900e4c6d7b77da71ed28b10
payamd <payam.d7@gmail.com>
2022-02-20 19:42:14 -0900

    Fix chrome requests

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 using Merchant_API.services;
+//var builder = WebApplication.CreateBuilder(args);
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                      });
+});
 
 // Add services to the container.
 
@@ -21,7 +34,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
