8846ba00cf1338801f3e9e72ffad6d904197f399
payamd <payam.d7@gmail.com>
2022-04-03 12:53:39 -0400

    null refrence bug fix

--- Models/MongoDBSettings.cs
+++ Models/MongoDBSettings.cs
@@ -13,4 +13,14 @@ public class MongoDBSettings{
         public string? UserCollectionName { get; set; }
 
 
+
+public MongoDBSettings()
+{
+    this.ConnectionString = "mongodb://payam:P123123123@merchant-shard-00-00.08y6e.mongodb.net:27017,merchant-shard-00-01.08y6e.mongodb.net:27017,merchant-shard-00-02.08y6e.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-tgydkl-shard-0&authSource=admin&retryWrites=true&w=majority";
+    this.DatabaseName= "Merchant";
+    this.ShopCollectionName="Shop";
+    this.ChatCollectionName="Chat";
+    this.ItemCollectionName="Item";
+    this.UserCollectionName="User";
+}
 }
